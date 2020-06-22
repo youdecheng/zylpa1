db56e96a1eb4194cbdc542f73698ee6301be02aa
youdecheng <183591156057@163.com>
2020-06-23 00:12:09 +0800

    代码生成器提交

---
+++ zylpa-generator/src/main/java/com/zylpa/ZylpaGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.zylpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZylpaGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZylpaGeneratorApplication.class, args);
+	}
+
+}
